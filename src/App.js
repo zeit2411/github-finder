ad2b714ce49ae8b87ca26f3e5c9e0f36ac0cdbb2
abcd8 <brianbui.n9@gmail.com>
2020-03-02 21:12:10 +0700

    add user item components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "./components/layouts/Navbar";
 import "./App.css";
+import UserItem from "./components/users/UserItem";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
