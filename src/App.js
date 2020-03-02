ab9fd849695eeecb053da9a721e16e0320e52108
abcd8 <brianbui.n9@gmail.com>
2020-03-02 21:54:06 +0700

    fetch github users api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import Navbar from "./components/layouts/Navbar";
+import axios from "axios";
 import "./App.css";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
